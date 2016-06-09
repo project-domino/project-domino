db222aca372de2fea415e6a66f51d7cb0d5b2f00
Nathan Ringo <remexre@gmail.com>
2016-06-08 22:34:38 -0500

    Adds a build script.

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Exit immediately on error.
+set -e;
+
+# Client-building function.
+function client() {
+	cd assets;
+	if [[ ! -e node_modules ]]; then
+		npm i;
+	else
+		gulp clean;
+		gulp;
+	fi;
+};
+
+# Server-building function.
+function server() {
+	go build;
+};
+
+# Realpath function for OS X people.
+# Credit https://stackoverflow.com/questions/3572105.
+command -v realpath >/dev/null 2>&1 || function realpath() {
+	[[ "${1}" = /* ]] && echo "${1}" || echo "$(pwd)/${1#./}";
+};
+
+# A simple function that builds, always from the base directory.
+base_dir="$(dirname $(realpath ${0}))";
+function build() {
+	cd "${base_dir}";
+	${@};
+};
+
+build client;
+build server;
