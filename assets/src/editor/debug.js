29759dea2b3de4928e359a0163aa32677d516aa9
Nathan Ringo <remexre@gmail.com>
2016-06-10 16:33:07 -0500

    Adds some JSDoc.

--- assets/src/editor/debug.js
+++ assets/src/editor/debug.js
@@ -5,16 +5,35 @@
 
 import SaveManager from "./save-manager.js";
 
+/**
+ * Returns a thenable function that returns a promise that allows a value to
+ * pass through after waiting for the given delay.
+ * @param {number} delay - The delay to add, in milliseconds.
+ * @return {module:debug~thenable}
+ */
 // eslint-disable-next-line no-unused-vars
 const slowPromise = delay => value => new Promise((resolve, reject) => {
 	window.setTimeout(() => resolve(value), delay);
 });
 
 /**
+ * A function that accepts zero or one parameters and returns a Promise.
+ * @callback thenable
+ * @param {*} [previous] - The result from the previous Promise, if any.
+ * @return {Promise}
+ */
+
+/**
  * A SlowSaveManager wraps another SaveManager and adds a configurable delay for
  * all operations.
+ * @extends module:save-manager~SaveManager
  */
 class SlowSaveManager extends SaveManager {
+	/**
+	 * Constructs a SlowSaveManager.
+	 * @param {module:save-manager~SaveManager} inner - The SaveManager to wrap.
+	 * @param {number} delay - The delay to add, in milliseconds.
+	 */
 	constructor(inner, delay = 1000) {
 		super();
 		this.inner = inner;
