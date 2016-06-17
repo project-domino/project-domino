f2aa417792cab7884c6d2beced2968dcdfdf812c
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2016-06-16 21:43:54 -0500

    select2 ui

--- assets/src/js/new-note.js
+++ assets/src/js/new-note.js
@@ -2,8 +2,7 @@ import $ from "jquery";
 import "select2";
 
 $(() => {
-	$("#tag-selector").select2({
-		tags:        "true",
+	$(".tag-selector").select2({
 		placeholder: "Type to search for tags...",
 		allowClear:  true,
 	});
