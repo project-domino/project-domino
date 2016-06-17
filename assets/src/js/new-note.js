5547d633e0081ebb077a935d35523076c1b74ea8
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2016-06-16 22:25:47 -0500

    new note page

--- assets/src/js/new-note.js
+++ assets/src/js/new-note.js
@@ -1,7 +1,10 @@
 import $ from "jquery";
 import "select2";
+import Editor from "../editor/editor.js";
 
 $(() => {
+	const editor = new Editor(document.querySelector(".new-note-editor"));
+	window.editor = editor;
 	$(".tag-selector").select2({
 		placeholder: "Type to search for tags...",
 		allowClear:  true,
