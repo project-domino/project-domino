db859417b84b14b42537c625bb1fb3fbf05514d3
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2016-06-24 00:33:01 -0500

    sass organization

--- middleware/load_user.go
+++ middleware/load_user.go
@@ -2,19 +2,18 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	"github.com/project-domino/project-domino/models"
+	"github.com/project-domino/project-domino/util"
 )
 
 // LoadUser loads certain objects in the request context's user
 func LoadUser(objects ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Acquire variables from request context.
-		db := c.MustGet("db").(*gorm.DB)
+		// Acquire user from the request context.
 		user := c.MustGet("user").(models.User)
 
 		// Set objects to be preloaded to db
-		preloadedDB := db.Where("id = ?", user.ID)
+		preloadedDB := util.DB.Where("id = ?", user.ID)
 		for _, object := range objects {
 			preloadedDB = preloadedDB.Preload(object)
 		}
