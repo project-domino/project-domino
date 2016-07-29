5d4c185f3d2fbda283191add1ff0000b2ceeca3d
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2016-07-29 17:59:31 -0500

    transactions for db setup

--- db/setup.go
+++ db/setup.go
@@ -1,50 +1,64 @@
 package db
 
-import "github.com/project-domino/project-domino/models"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/project-domino/project-domino/models"
+)
 
 // Setup initializes the database with empty tables of all the needed
 // types.
 // TODO weights on searchtext
 func Setup() error {
-	if !DB.HasTable(&models.User{}) {
-		DB.CreateTable(&models.User{})
-		DB.Exec("ALTER TABLE users ADD COLUMN searchtext TSVECTOR")
-		DB.Exec("CREATE INDEX searchtext_user_gin ON users USING GIN(searchtext)")
-		DB.Exec(`CREATE TRIGGER ts_searchtext_user
+	// Create transaction
+	DB.Exec("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE")
+	tx := DB.Begin()
+	defer tx.Commit()
+
+	if !tx.HasTable(&models.User{}) {
+		tx.CreateTable(&models.User{})
+		tx.Exec("ALTER TABLE users ADD COLUMN searchtext TSVECTOR")
+		tx.Exec("CREATE INDEX searchtext_user_gin ON users USING GIN(searchtext)")
+		tx.Exec(`CREATE TRIGGER ts_searchtext_user
 			BEFORE INSERT OR UPDATE ON users
 			FOR EACH ROW EXECUTE PROCEDURE
 			tsvector_update_trigger('searchtext', 'pg_catalog.english', 'user_name')`)
 	}
-	if !DB.HasTable(&models.Note{}) {
-		DB.CreateTable(&models.Note{})
-		DB.Exec("ALTER TABLE notes ADD COLUMN searchtext TSVECTOR")
-		DB.Exec("CREATE INDEX searchtext_note_gin ON notes USING GIN(searchtext)")
+	if !tx.HasTable(&models.Note{}) {
+		tx.CreateTable(&models.Note{})
+		tx.Exec("ALTER TABLE notes ADD COLUMN searchtext TSVECTOR")
+		tx.Exec("CREATE INDEX searchtext_note_gin ON notes USING GIN(searchtext)")
 	}
-	if !DB.HasTable(&models.Collection{}) {
-		setupTable(&models.Collection{})
-		DB.Exec("ALTER TABLE collections ADD COLUMN searchtext TSVECTOR")
-		DB.Exec("CREATE INDEX searchtext_collection_gin ON collections USING GIN(searchtext)")
+	if !tx.HasTable(&models.Collection{}) {
+		tx.CreateTable(&models.Collection{})
+		tx.Exec("ALTER TABLE collections ADD COLUMN searchtext TSVECTOR")
+		tx.Exec("CREATE INDEX searchtext_collection_gin ON collections USING GIN(searchtext)")
 	}
-	if !DB.HasTable(&models.Tag{}) {
-		setupTable(&models.Tag{})
-		DB.Exec("ALTER TABLE tags ADD COLUMN searchtext TSVECTOR")
-		DB.Exec("CREATE INDEX searchtext_tag_gin ON tags USING GIN(searchtext)")
-		DB.Exec(`CREATE TRIGGER ts_searchtext_tag
+	if !tx.HasTable(&models.Tag{}) {
+		tx.CreateTable(&models.Tag{})
+		tx.Exec("ALTER TABLE tags ADD COLUMN searchtext TSVECTOR")
+		tx.Exec("CREATE INDEX searchtext_tag_gin ON tags USING GIN(searchtext)")
+		tx.Exec(`CREATE TRIGGER ts_searchtext_tag
 			BEFORE INSERT OR UPDATE ON tags
 			FOR EACH ROW EXECUTE PROCEDURE
 			tsvector_update_trigger('searchtext', 'pg_catalog.english', 'name', 'description')`)
 	}
-	setupTable(&models.AuthToken{})
-	setupTable(&models.Comment{})
-	setupTable(&models.CollectionNote{})
-	setupTable(&models.Email{})
+	setupTable(&models.AuthToken{}, tx)
+	setupTable(&models.Comment{}, tx)
+	setupTable(&models.CollectionNote{}, tx)
+	setupTable(&models.Email{}, tx)
 
-	return DB.Error
+	err := tx.Error
+
+	if err != nil {
+		tx.Rollback()
+	}
+
+	return err
 }
 
 // Creates a table for a specified struct if one doesn't exist
-func setupTable(val interface{}) {
-	if !DB.HasTable(val) {
-		DB.CreateTable(val)
+func setupTable(val interface{}, db *gorm.DB) {
+	if !db.HasTable(val) {
+		db.CreateTable(val)
 	}
 }
