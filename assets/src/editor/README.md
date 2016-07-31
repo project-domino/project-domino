172bf3b8f6f51a455dd552fe4275396146d76790
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2016-07-31 16:55:59 -0500

    removed editor

--- assets/src/editor/README.md
+++
@@ -1,3 +0,0 @@
-# project-domino-editor
-
-This is the note-editing component used in Project Domino.
--- assets/src/editor/main.js
+++
@@ -1,9 +0,0 @@
-import Editor from "./editor.js";
-import {DebugSaveManager} from "./save-manager.js";
-import {SlowSaveManager} from "./debug.js";
-
-const saveManager = new SlowSaveManager(new DebugSaveManager());
-const editor = new Editor(document.getElementById("editor"), saveManager);
-
-window.editor = editor;
-export default editor;
